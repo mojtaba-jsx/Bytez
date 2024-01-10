0fa2b73eee04408c498786fa07528bde22e71a40
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-01-10 14:23:11 +0330

    [htm-css]- add sign up sign in box and change header section

--- src/js/shared.js
+++ src/js/shared.js
@@ -1,17 +1,36 @@
 //! Element Variables
 const mobileMenuBtn = document.querySelector('.mobile-menu__btn');
+
 const mobileMenu = document.querySelector('.mobile-menu');
 
 
 //! Logic For Show And Hide Mobile Menu
-let flag = false;
+let flag1 = false;
 mobileMenuBtn.addEventListener('click',()=>{
-    if(!flag){
+    if(!flag1){
         mobileMenu.style.display = 'block'
-        flag=true;
+        flag1=true;
     }else{
         mobileMenu.style.display = 'none'
-        flag=false;
+        flag1=false;
 
     }
 })
+
+
+const searchIconsUserSvg = document.querySelector('.search-icons__user-svg');
+const signinSignup = document.querySelector('.signin-signup')
+let flag2 = false
+searchIconsUserSvg.addEventListener('click',()=>{
+    if(!flag2){
+        signinSignup.style.visibility = 'visible';
+        flag2 = true;
+    }else{
+        signinSignup.style.visibility = 'hidden';
+        flag2 = false;
+    }
+
+})
+
+
+
