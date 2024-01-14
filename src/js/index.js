88e474f8611bb40c46f044130178ca780cfa62a5
mojtaba.jsx <mojtaba.jsx@gmail.com>
2024-01-14 12:24:54 +0330

    [htm-css]- add slider to recommended section

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('.recommended__wrapper').slick({
+        centerMode: true,
+        infinite:true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        variableWidth: true,
+        focusOnSelect:true,
+        infinite:true,
+        autoplay:true,
+        autoplaySpeed:2000,
+    });
+  });
+
+
+
